7d0b40375f9fdfc8d159a3c717a459eb677cbdc8
simon123h <simon123h@users.noreply.github.com>
2018-02-07 17:20:19 +0100

    Add more keybindings

--- js/keybindings.js
+++ js/keybindings.js
@@ -4,22 +4,70 @@
 var Key = {
   _pressed: {},
 
-  // TODO: add more keys
-  LEFT: 37,
-  UP: 38,
-  RIGHT: 39,
-  DOWN: 40,
-  M: 77,
-  W: 87,
   A: 65,
-  S: 83,
+  B: 66,
+  C: 67,
   D: 68,
-  Q: 81,
+  E: 69,
+  F: 70,
+  G: 71,
+  H: 72,
   I: 73,
   J: 74,
   K: 75,
   L: 76,
+  M: 77,
+  N: 78,
+  O: 79,
+  P: 80,
+  Q: 81,
+  R: 82,
+  S: 83,
+  T: 84,
   U: 85,
+  V: 86,
+  W: 87,
+  X: 88,
+  Y: 89,
+  Z: 90,
+  R0: 48,
+  R1: 49,
+  R2: 50,
+  R3: 51,
+  R4: 52,
+  R5: 53,
+  R6: 54,
+  R7: 55,
+  R8: 56,
+  R9: 57,
+  N0: 96,
+  N1: 97,
+  N2: 98,
+  N3: 99,
+  N4: 100,
+  N5: 101,
+  N6: 102,
+  N7: 103,
+  N8: 104,
+  N9: 105,
+  LEFT: 37,
+  UP: 38,
+  RIGHT: 39,
+  DOWN: 40,
+  COMMA: 188,
+  DASH: 189,
+  PERIOD: 190,
+  LL: 220,
+  SHIFT: 16,
+  CAPSLOCK: 20,
+  TAB: 9,
+  CARET: 192,
+  ALT: 18,
+  CTRL: 17,
+  ENTER: 13,
+  SPACE: 32,
+  BACKSPACE: 8,
+  ESCAPE: 27,
 
 
   isDown: function(keyCode) {
@@ -37,10 +85,20 @@ var Key = {
 
 // available keymaps
 // order: up, left, down, right, fire
+// var keymaps = [
+//   [Key.UP, Key.LEFT, Key.DOWN, Key.RIGHT, Key.M],
+//   [Key.W, Key.A, Key.S, Key.D, Key.Q],
+//   [Key.T, Key.F, Key.G, Key.H, Key.R],
+//   [Key.I, Key.J, Key.K, Key.L, Key.U]
+// ]
 var keymaps = [
-  [Key.UP, Key.LEFT, Key.DOWN, Key.RIGHT, Key.M],
+  [Key.UP, Key.LEFT, Key.DOWN, Key.RIGHT, Key.CTRL],
   [Key.W, Key.A, Key.S, Key.D, Key.Q],
-  [Key.I, Key.J, Key.K, Key.L, Key.U]
+  [Key.F, Key.C, Key.V, Key.B, Key.X],
+  [Key.Z, Key.G, Key.H, Key.J, Key.T],
+  [Key.K, Key.M, Key.COMMA, Key.PERIOD, Key.N],
+  [Key.N4, Key.E, Key.R, Key.N5, Key.N3],
+  [Key.N8, Key.U, Key.I, Key.N9, Key.N7]
 ]
 
 // event listeners
