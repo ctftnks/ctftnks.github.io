f08efd65020c29ed568b61b96263e74ee07fe956
simon123h <simon123h@users.noreply.github.com>
2018-03-30 15:02:08 +0200

    Make mobile controls work

--- js/mobile.js
+++ js/mobile.js
@@ -7,8 +7,59 @@ function checkMobile(){
 }
 
 
+mobileControlsIntvl = 0;
 
 enableMobile = function(){
+
   console.log("mobile controls activated!");
-  document.getElementById("mobileControls").style.display = "block";
+
+
+
+  // include joystick script
+  var script = document.createElement("script");
+  script.src = "js/joystick.js";
+  script.onload = function(){
+
+    document.getElementById("mobileControls").style.display = "block";
+    var joystick	= new VirtualJoystick({
+      container	: document.getElementById('joystickContainer'),
+      mouseSupport	: true,
+    });
+
+    mobileControlsIntvl = setInterval(function(){
+
+      if(typeof(game) == "undefined")
+        return;
+
+
+        var t = game.players[0].tank;
+      var jx = joystick.deltaX();
+      var jy = joystick.deltaY();
+      var angle = Math.atan2(jy, jx) + Math.PI / 2;
+      t.angle = angle;
+
+      if(jx*jx+jy*jy > 5)
+        Key._pressed[Key.UP] = true;
+      else
+        Key._pressed[Key.UP] = false;
+
+    }, GameFrequency);
+
+  };
+  document.head.appendChild(script);
+
+
+  document.getElementById("mobileFire").ontouchstart = function(){
+    Key._pressed[Key.SPACE] = true;
+  }
+  document.getElementById("mobileFire").ontouchend = function(){
+    Key._pressed[Key.SPACE] = false;
+  }
+  document.getElementById("mobileBack").ontouchstart = function(){
+    Key._pressed[Key.DOWN] = true;
+  }
+  document.getElementById("mobileBack").ontouchend = function(){
+    Key._pressed[Key.DOWN] = false;
+  }
+
 }
