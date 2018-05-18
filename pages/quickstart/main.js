773e7925380a382eb1e4010a6d701ebbea189be9
simon123h <simon123h@users.noreply.github.com>
2018-05-18 15:06:15 +0200

    Add Uneven Mixed mode

--- pages/quickstart/main.js
+++ pages/quickstart/main.js
@@ -59,3 +59,22 @@ function quickMixed(nteams, teamsize){
   }
   updatePlayersMenu();
 }
+
+function quickUnevenMixed(nteams, teamsize){
+  players = [];
+  nplayers = 0;
+  for(var i=0; i<nteams; i++){
+    for(var j=0; j<teamsize; j++){
+      if(j < teamsize/2 && i == 0)
+        players.push(new Player());
+      else
+        players.push(new Bot());
+      var p = players[players.length-1];
+      if(j>0){
+        p.team = players[players.length-2].team;
+        p.color = players[players.length-2].color;
+      }
+    }
+  }
+  updatePlayersMenu();
+}
