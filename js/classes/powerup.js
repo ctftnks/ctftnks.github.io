9e83f43d93206979272d6171d27f263c0f5aca73
simon123h <simon123h@users.noreply.github.com>
2019-03-02 18:22:13 +0100

    Fix Terminator bonus frequency

--- js/classes/powerup.js
+++ js/classes/powerup.js
@@ -147,14 +147,12 @@ TerminatorBonus = function () {
     this.applied = true;
     playSound("res/sound/terminator.mp3");
     var self = tank;
-    tank.defaultWeapon = function () { }
-    self.weapon.image = this.image;
-    var intvl = setInterval(function () {
+    tank.defaultWeapon = function () {
       self.weapon.canShoot = true;
       self.weapon.fired = false;
-    }, 1000);
+    }
+    self.weapon.image = this.image;
     tank.player.game.timeouts.push(setTimeout(function () {
-      clearInterval(intvl);
       self.defaultWeapon = function () {
         self.weapon = new Gun(self);
       };
