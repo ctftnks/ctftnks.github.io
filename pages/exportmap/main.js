0c4c82a40cd124881a88b463596145e0c92710e9
simon123h <simon123h@users.noreply.github.com>
2025-12-30 14:51:23 +0100

    refactor: remove unused map import/export feature

--- pages/exportmap/main.js
+++
@@ -1,7 +0,0 @@
-import template from "./main.html?raw";
-import "./style.css";
-
-export function init(container) {
-  container.innerHTML = template;
-  document.getElementById("exportMapText").innerHTML = MapGenerator.exportMap(game.map);
-}
