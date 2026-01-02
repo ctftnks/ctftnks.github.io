2c566216fc4f5009ecb19afc27420ceac14ce50c
simon123h <simon123h@users.noreply.github.com>
2026-01-02 17:56:20 +0100

    config: add ESLint

---
+++ eslint.config.mjs
@@ -0,0 +1,43 @@
+import globals from "globals";
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier, { rules } from "eslint-config-prettier";
+import jsdoc from "eslint-plugin-jsdoc";
+
+export default [
+  {
+    ignores: ["dist/**", "node_modules/**", "coverage/**", "public/**"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+
+    plugins: {
+      jsdoc,
+    },
+
+    languageOptions: {
+      globals: globals.browser,
+    },
+
+    rules: {
+      // Enforce JSDoc comments for functions and methods
+      "jsdoc/require-jsdoc": [
+        "warn",
+        {
+          require: {
+            ArrowFunctionExpression: false,
+            ClassDeclaration: true,
+            ClassExpression: true,
+            FunctionDeclaration: true,
+            FunctionExpression: true,
+            MethodDefinition: true,
+          },
+          enableFixer: false,
+        },
+      ],
+    },
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+];
